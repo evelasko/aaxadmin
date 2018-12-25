3b0f60ed8044fac3cc3bac91e9c020af57b8f98a
Enrique Velasco <h.superpotter@gmail.com>
2018-12-25 21:39:12 +0100

    setup web admin with apollo and react router

---
+++ packages/aaxadminweb/src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+
+// import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { client } from './apollo'
+import { Routes } from './routes';
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+  <Routes />
+  </ApolloProvider>,
+  document.getElementById('root') as HTMLElement
+)
+
+registerServiceWorker();
